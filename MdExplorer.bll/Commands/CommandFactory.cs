bb369cb2444cddcca04e4513b6f840ab1e22bd5c
Carlo Salaroglio <salaroglio@hotmail.com>
2021-07-01 14:01:54 +0200

    wip Commands structure

---
+++ MdExplorer.bll/Commands/CommandFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MdExplorer.Features.Commands
+{
+    public class CommandFactory : ICommandFactory
+    {
+        static object lockGetCommands;
+        public ICommand[] GetCommands()
+        {
+            // E' facile, tiro su tutte le classi presenti dentro ICommand,
+            // le instanzio e le butto dentro all'array
+            lock (lockGetCommands)
+            {
+                var listToReturn = new List<ICommand>();
+                var messengers = Assembly.GetExecutingAssembly().GetTypes().Where(_ => (typeof(ICommand).IsAssignableFrom(_)));
+                foreach (var item in messengers)
+                {
+                    listToReturn.Add((ICommand)Activator.CreateInstance(item));
+                }
+                return listToReturn.ToArray();
+            }            
+        }
+    }
+}
---
+++ MdExplorer.bll/Commands/MDShowMD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MdExplorer.Features.Commands
+{
+    public class MDShowMD : ICommand
+    {
+        public string TransformInNewMDFromMD(string markdown)
+        {
+            // Devo cercare dentro markdown il comando m↓show-md(pathfile)
+            // potrei usare le regular expression
+            return markdown;
+        }
+    }
+}
