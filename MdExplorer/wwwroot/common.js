8d279be1b6159d6366ca23abad7b0f8406678a00
salaroglio <salaroglio@hotmail.com>
2021-09-20 15:00:37 +0200

    fix to manage embedded files

--- MdExplorer/wwwroot/common.js
+++ MdExplorer/wwwroot/common.js
@@ -1,6 +1,6 @@
 ﻿document.write("<script src='/bootstrap/jquery-3.6.0.js'></script>");
-document.write("<script src='/bootstrap-datepicker/js/bootstrap-datepicker.js'></script>");
 document.write("<script src='/bootstrap/js/bootstrap.bundle.js'></script>");
+document.write("<script src='/bootstrap_datepicker/js/bootstrap-datepicker.js'></script>");
 document.write("<script src='/javascripts/jqueryForFirstPage.js'></script>");
 
 
