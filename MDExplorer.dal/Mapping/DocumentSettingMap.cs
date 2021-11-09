0e790160a73a5a9363036cc70ff21a73dcdfd7d9
salaroglio <salaroglio@hotmail.com>
2021-11-09 15:15:31 +0100

    store in usersettings show/hide TOC for document

---
+++ MDExplorer.dal/Mapping/DocumentSettingMap.cs
@@ -0,0 +1,21 @@
+﻿using FluentNHibernate.Mapping;
+using MdExplorer.Abstractions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDExplorer.DataAccess.Mapping
+{
+    public class DocumentSettingMap : ClassMap<DocumentSetting>
+    {
+        public DocumentSettingMap()
+        {
+            Table("DocumentSetting");
+            Id(_ => _.Id).GeneratedBy.GuidComb();
+            Map(_ => _.ShowTOC).Not.Nullable();
+            Map(_ => _.DocumentPath).Not.Nullable();
+        }
+    }
+}
---
+++ MdExplorer.Abstractions/Models/DocumentSetting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MdExplorer.Abstractions.Models
+{
+    public class DocumentSetting
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string DocumentPath { get; set; }
+        public virtual bool ShowTOC { get; set; }
+    }
+}
