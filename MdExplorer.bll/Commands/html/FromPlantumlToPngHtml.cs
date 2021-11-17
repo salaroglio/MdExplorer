d2bee379b47dfc5e416aa41c519bf7e2430829fb
salaroglio <salaroglio@hotmail.com>
2021-11-17 03:43:52 +0100

    presentation plantuml feature

--- MdExplorer.bll/Commands/html/FromPlantumlToPngHtml.cs
+++ MdExplorer.bll/Commands/html/FromPlantumlToPngHtml.cs
@@ -72,15 +72,22 @@ namespace MdExplorer.Features.Commands.html
                     var presentationPlantuml = matchesMd[0].Groups[0].Value;
                     var calculatedPresentation = string.Empty;
                     var completedPlantuml = string.Empty;
-                    totalStep = matchesStep.Count();
+                    
                     var matchesStepList = matchesStep.ToList();
+                    totalStep = matchesStepList.Max(_=>Convert.ToInt32(_.Groups[1].Value));
 
+                    Match currentProcessedStep = null;
                     foreach (var itemStep in matchesStepList)
                     {
+                        if (currentProcessedStep != null && Convert.ToInt32( currentProcessedStep.Groups[1].Value) >= Convert.ToInt32( itemStep.Groups[1].Value) )
+                        {
+                            continue;
+                        }
+                         currentProcessedStep = itemStep;
                         if (itemStep.Groups[2].Value == string.Empty)
                         {
                             var stepIndex = matchesStepList.IndexOf(itemStep);
-                            if (stepIndex < matchesStepList.Count-1) // it's not the last
+                            if (stepIndex < matchesStepList.Count - 1) // it's not the last
                             {
                                 var nextStep = matchesStepList[stepIndex + 1];
                                 calculatedPresentation += presentationPlantuml.Substring(itemStep.Index, nextStep.Index - itemStep.Index);
@@ -91,9 +98,34 @@ namespace MdExplorer.Features.Commands.html
                                 completedPlantuml = firstPartPlantuml + calculatedPresentation;
                             }
                         }
-                        if (Convert.ToInt32(itemStep.Groups[1].Value) == step)
+                        if (itemStep.Groups[2].Value == "Nested")
                         {
-                            if (step == matchesStepList.Count)
+                            var stepIndex = matchesStepList.IndexOf(itemStep);
+                            var nextStep = matchesStepList[stepIndex + 1];
+                            calculatedPresentation += presentationPlantuml.Substring(itemStep.Index, nextStep.Index - itemStep.Index);
+                            if (nextStep.Groups[2].Value == "Nested")
+                            {
+                                if (Convert.ToInt32(itemStep.Groups[1].Value) != step)
+                                {
+                                    (var recursivePresentation, var lastStepNode, var lastProcessedStep) = RecursiveNestedStep(nextStep, matchesStepList, presentationPlantuml, step);
+                                    currentProcessedStep = lastProcessedStep;
+                                    calculatedPresentation += recursivePresentation;
+                                    calculatedPresentation += ClosingTag(presentationPlantuml, matchesStepList, itemStep, nextStep);
+                                }
+                                else
+                                {
+                                    calculatedPresentation += ClosingTag(presentationPlantuml, matchesStepList, itemStep, nextStep);
+                                }
+
+                            }
+
+                        }
+
+                        // Exit Strategy
+                        if (Convert.ToInt32(currentProcessedStep.Groups[1].Value) == step)
+                        {
+                            var indexStep = matchesStepList.IndexOf(itemStep);
+                            if (indexStep == matchesStepList.Count)
                             {
                                 completedPlantuml = firstPartPlantuml + calculatedPresentation;
                                 break;
@@ -108,7 +140,7 @@ namespace MdExplorer.Features.Commands.html
 
 
 
-                    
+
                     var textHashPresentation = _helper.GetHashString(completedPlantuml, Encoding.UTF8);
                     hashPresentationToReturn = textHashPresentation;
                     var filePathPng = $"{directoryInfo.FullName}{Path.DirectorySeparatorChar}{textHashPresentation}.svg";
@@ -118,26 +150,7 @@ namespace MdExplorer.Features.Commands.html
                         taskSvg.Wait();
                         var res = taskSvg.Result;
                         File.WriteAllBytes(filePathPng, res);
-                        XmlDocument doc = new XmlDocument();
-                        doc.Load(filePathPng);
-                        var nodeToParse = doc.LastChild;
-                        XmlNamespaceManager m = new XmlNamespaceManager(doc.NameTable);
-                        m.AddNamespace("myns", "http://www.w3.org/2000/svg");
-
-                        var nodeList = nodeToParse.SelectNodes("//myns:a", m);
-                        foreach (XmlNode itemNode in nodeList)
-                        {
-                            for (int i = itemNode.Attributes.Count - 1; i > 0; i--)
-                            {
-                                XmlAttribute itemAttribute = itemNode.Attributes[i];
-                                if (itemAttribute.Name != "href")
-                                {
-                                    itemNode.Attributes.Remove(itemAttribute);
-                                }
-                            }
-                        }
-                        var toReplace = nodeToParse.OuterXml;
-                        File.WriteAllText(filePathPng, toReplace);
+                        CleanSvg(filePathPng);
                         _logger.LogInformation($"write file: {filePathPng}");
                     }
                     break;// exit from cicle of markdown
@@ -145,6 +158,72 @@ namespace MdExplorer.Features.Commands.html
             }
 
             return ($".md/{hashPresentationToReturn}.svg?time={DateTime.Now.Ticks}", totalStep);
+        }
+
+        private static string ClosingTag(string presentationPlantuml, List<Match> matchesStepList, Match itemStep, Match nextStep)
+        {
+            var lastStep = matchesStepList.Where(_ => _.Groups[1].Value == nextStep.Groups[1].Value
+                       && _.Groups[2].Value == "End").FirstOrDefault();
+            var lastStepPlusOne = matchesStepList.Where(_ => _.Groups[1].Value == itemStep.Groups[1].Value
+                       && _.Groups[2].Value == "End").FirstOrDefault();            
+            return presentationPlantuml.Substring(lastStep.Index, lastStepPlusOne.Index - lastStep.Index);
+        }
+
+        private (string, Match,Match) RecursiveNestedStep(Match itemStep, List<Match> matchesStepList, string presentationPlantuml, int step)
+        {
+            var currentProcessedStep = itemStep;
+            var stepIndex = matchesStepList.IndexOf(itemStep);
+            var nextStep = matchesStepList[stepIndex + 1];
+            var stepNodeToReturn = nextStep;
+            var calculatedPresentation = presentationPlantuml.Substring(itemStep.Index, nextStep.Index - itemStep.Index);
+            if (nextStep.Groups[2].Value == "Nested")
+            {
+                if (Convert.ToInt32(itemStep.Groups[1].Value) != step)
+                {
+                    (var recursivePresentation, var lastStepNode, var lastProcessedStep) = RecursiveNestedStep(nextStep, matchesStepList, presentationPlantuml, step);
+                    currentProcessedStep = lastProcessedStep;
+                    calculatedPresentation += recursivePresentation;
+                    var indexLastStepNode = matchesStepList.IndexOf(lastStepNode);
+                    var nextLastStepnode = matchesStepList[indexLastStepNode + 1];
+                    recursivePresentation += presentationPlantuml.Substring(lastStepNode.Index, nextLastStepnode.Index - lastStepNode.Index);
+                    calculatedPresentation += recursivePresentation;
+                    stepNodeToReturn = nextLastStepnode;
+                }
+                else
+                {
+                    var lastStep = matchesStepList.Where(_ => _.Groups[1].Value == nextStep.Groups[1].Value
+                               && _.Groups[2].Value == "End").FirstOrDefault();
+                    var lastStepPlusOne = matchesStepList.Where(_ => _.Groups[1].Value == itemStep.Groups[1].Value
+                               && _.Groups[2].Value == "End").FirstOrDefault();
+                    calculatedPresentation += presentationPlantuml.Substring(lastStep.Index, lastStepPlusOne.Index - lastStep.Index);
+                }
+            }
+
+            return (calculatedPresentation, nextStep, currentProcessedStep);
+        }
+
+        private void CleanSvg(string filePathPng)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filePathPng);
+            var nodeToParse = doc.LastChild;
+            XmlNamespaceManager m = new XmlNamespaceManager(doc.NameTable);
+            m.AddNamespace("myns", "http://www.w3.org/2000/svg");
+
+            var nodeList = nodeToParse.SelectNodes("//myns:a", m);
+            foreach (XmlNode itemNode in nodeList)
+            {
+                for (int i = itemNode.Attributes.Count - 1; i > 0; i--)
+                {
+                    XmlAttribute itemAttribute = itemNode.Attributes[i];
+                    if (itemAttribute.Name != "href")
+                    {
+                        itemNode.Attributes.Remove(itemAttribute);
+                    }
+                }
+            }
+            var toReplace = nodeToParse.OuterXml;
+            File.WriteAllText(filePathPng, toReplace);
         }
 
         private (MatchCollection, MatchCollection) GetMatchesStep(string plantuml)
@@ -155,7 +234,7 @@ namespace MdExplorer.Features.Commands.html
             MatchCollection matchesStep = null;
             if (matchesMd.Count() > 0)
             {
-                var search = @"'Step([1-9]+)(Nested|End)?";
+                var search = @"'Step([0-9]+)(Nested|End)?";
                 Regex rx1 = new Regex(search, RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
                 matchesStep = rx1.Matches(matchesMd[0].Groups[0].Value);
             }
