31ca02a665721a00eb77d6f39098d1cfac9d3b8d
salaroglio <salaroglio@hotmail.com>
2022-04-22 22:37:59 +0200

    implementing tocbot

--- MdExplorer/wwwroot/common.js
+++ MdExplorer/wwwroot/common.js
@@ -1,10 +1,7 @@
 ﻿document.write("<script src='/bootstrap/jquery-3.6.0.js'></script>");
 document.write("<script src='/bootstrap/js/bootstrap.bundle.js'></script>");
 
-
 document.write("<script src='/jquery_ui/jquery-ui.js'></script>");
 document.write("<script src='/bootstrap_datepicker/js/bootstrap-datepicker.js'></script>");
 document.write("<script src='/javascripts/jqueryForFirstPage.js'></script>");
-
-    
-    
+document.write("<script src='/TocBot/tocbot.min.js'></script>");
