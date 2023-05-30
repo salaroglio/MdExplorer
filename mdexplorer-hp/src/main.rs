59a4aeafb420595344c5eba8592407ab6566d6e9
Carlo <salaroglio@hotmail.com>
2023-05-30 09:43:53 +0200

    test vari

--- mdexplorer-hp/src/main.rs
+++ mdexplorer-hp/src/main.rs
@@ -1,20 +1,24 @@
 use ferris_says::say;
 use std::io::{stdout,BufWriter};
 use std::fs;
-
+use regex::Regex;
 
 fn main() {
-    let readSomething = fs::read_to_string("C:\\Users\\Carlo\\Documents\\0-progetti\\artigiancassa\\artigiancassadocs\\analisi-as-is\\AS400-StoredProcedures.md");    
-    let currentFile = match readSomething{
+    
+    //let file_path = ;
+    let read_something = fs::read_to_string("C:\\Users\\Carlo\\Documents\\0-progetti\\artigiancassa\\artigiancassadocs\\analisi-as-is\\AS400-StoredProcedures.md");
+    let current_file = match read_something{
                     Ok(file)=>file,
-                    Err(error)=>panic!("prolema di lettura: {:?}",error)};
-    
-    
+                    Err(error)=>panic!("problema di lettura: {:?}",error)};
+        
+    let re = Regex::new(r"[^!]\[[^\]]*\]\(([^\)]*)\)").unwrap();
+    re.find(current_file.as_str());
 
+    
     let stdout = stdout();
     let message = String::from("Hello fellow Rustaceans!");
     let width = message.chars().count();
     let mut writer = BufWriter::new(stdout.lock());
     say(message.as_str(),width,&mut writer).unwrap();
-    println!("Hello, world!");
+    println!("Hello, world again!");
 }
