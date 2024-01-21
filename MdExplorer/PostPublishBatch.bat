1a65f04bad8ba5d763c0fcdcbf34915c00d69179
Carlo Salaroglio <salaroglio@hotmail.com>
2024-01-21 21:13:21 +0100

    fix electron app wasn't show

--- MdExplorer/PostPublishBatch.bat
+++ MdExplorer/PostPublishBatch.bat
@@ -1,4 +1,3 @@
 copy Binaries\Plantuml.jar bin\publish\Plantuml.jar
 copy Binaries\GraphWiz\binaries\dot.exe bin\publish\dot.exe
-copy ElectronMdExplorer\release-builds\ElectronMdExplorer-win32-x64 Binaries\ElectronMdExplorer
 copy Binaries\ElectronMdExplorer bin\publish\ElectronMdExplorer 
