aec2442d737d9341c0a0224ff37adbdb1b9c5167
Carlo Salaroglio <salaroglio@hotmail.com>
2024-01-28 11:56:47 +0100

    Fix: missing SignalR connectionId into links

--- MdExplorer.bll/Commands/ManageLinkAbsolutePath.cs
+++ MdExplorer.bll/Commands/ManageLinkAbsolutePath.cs
@@ -3,11 +3,15 @@ using MdExplorer.Features.Commands.html;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
 namespace MdExplorer.Features.Commands
 {
+    /// <summary>
+    /// Manage absolute path and and add SignalR connectionId
+    /// </summary>
     internal class ManageLinkAbsolutePath : ICommand
     {
         private readonly ILogger<ManageLinkAbsolutePath> _logger;
@@ -50,6 +54,16 @@ namespace MdExplorer.Features.Commands
                 if (link.Groups[2].Value.StartsWith("/"))
                 {
                     var newlink = "/api/mdexplorer" + link.Groups[2].Value;
+
+                    Regex rxSharp = new Regex(@"([^#]*)(?:(#.*))?",
+                             RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
+                    var matchesSharp = rxSharp.Matches(newlink);
+
+                    var firstPart = matchesSharp.First().Groups[1].Value;
+                    var secondPart = matchesSharp.First().Groups[2]?.Value;
+
+                    newlink = $@"{firstPart}" + "?connectionId=" + requestInfo.ConnectionId + secondPart;
+
                     var allElementToReplace = link.Groups[0].Value.Replace(link.Groups[2].Value, newlink);
                     markdown = markdown.Replace(link.Groups[0].Value, allElementToReplace);
                 }
