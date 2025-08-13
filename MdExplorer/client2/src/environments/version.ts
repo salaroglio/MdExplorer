acfe05bdbd958d1e8adeadfe608ed5875814fe38
Carlo Salaroglio <salaroglio@hotmail.com>
2025-08-13 09:48:02 +0200

    ultima compilazione angular

--- MdExplorer/client2/src/environments/version.ts
+++ MdExplorer/client2/src/environments/version.ts
@@ -1,6 +1,6 @@
 // Questo file è generato automaticamente dallo script update-version.js
 // Non modificarlo manualmente.
 export const versionInfo = {
-  version: '2025.08.13.3',
-  buildTime: '2025.08.13 08:49:21'
+  version: '2025.08.13.4',
+  buildTime: '2025.08.13 09:33:57'
 };
